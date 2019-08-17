8acb0b24f76040c5a35d971cf79478ffcb2de48e
OldDriver9527 <1187557502@qq.com>
2019-08-17 21:05:55 +0800

    添加属性占位符笔记

---
+++ src/main/java/org/olddriver/spring/LearnPropertyPlaceHolderAndSpEL.java
@@ -0,0 +1,16 @@
+package org.olddriver.spring;
+
+public class LearnPropertyPlaceHolderAndSpEL {
+	/*
+	 * 通过属性占位符，spring表达式语言
+	 * 为bean中基本类型数据注入值
+	 * 
+	 * 属性占位符
+	 * Environment 集成在spring容器中的对象，其中存储当前应用的环境信息，如系统环境变量，jvm的属性等
+	 * 通过@PropertySource注解，可以将指定properties文件对应的PropertySource对象加入Environment中
+	 * 属性占位符会被解析为Environment中存储的属性值
+	 * 
+	 * 
+	 * 
+	 */
+}
