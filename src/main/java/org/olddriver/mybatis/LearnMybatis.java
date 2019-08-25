4b4f40decc967712f49da97522f666087c24ca91
OldDriver9527 <1187557502@qq.com>
2019-08-25 18:46:50 +0800

    添加mybatis

---
+++ src/main/java/org/olddriver/mybatis/LearnMybatis.java
@@ -0,0 +1,15 @@
+package org.olddriver.mybatis;
+/**
+ * 
+ * mybatis 笔记
+ *
+ */
+public class LearnMybatis {
+	/*
+	 * mybtais 
+	 * 一款基于java的持久层框架，前身为iBATIS
+	 * mybatis使用原生sql进行操作，提供优化sql的机会
+	 * 
+	 * 
+	 */
+}
